ca64860f9f7c53d2f7446308bbb388cb734354de
Liyijia <liyijia@gamil.com>
2020-03-26 21:51:12 +0300

    change + q

--- Bucket_Sort.h
+++ Bucket_Sort.h
@@ -8,7 +8,7 @@ using namespace std;
 void BucketSort(int* arr, int len);
 void SelectSort(vector<int>& v);
 
-//External interface
+//External interfaceq
 void BucketSort(int* arr, int len)
 {
 	//initialize vector
