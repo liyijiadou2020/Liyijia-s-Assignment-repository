eae6c40fab5e4ed2e99ac187b6e1cfb8541f0274
liyijiadou2020 <62644469+liyijiadou2020@users.noreply.github.com>
2020-05-10 11:58:07 +0300

    Update Dictionary.h

--- 02Final assignment - frequency dictionary/Dictionary.h
+++ 02Final assignment - frequency dictionary/Dictionary.h
@@ -16,317 +16,305 @@ bool cmp_test(const pair<string, int>& a, const pair<string, int>& b);
 
 class Dictionary {
 private:
-    struct Node {
+  struct Node {
 
-        string key;
-        int frequency;
-        Node* left;
-        Node* right;
+    string key;
+    int frequency;
+    Node* left;
+    Node* right;
 
-        Node(string key) {
-            this->key = key;
-            this->left = this->right = NULL;
-            this->frequency = 1;
-        }
-        Node(Node* node) {
-            this->key = node->key;
-            this->right = node->right;
-            this->left = node->left;
-            this->frequency = node->frequency;
-        }
-        Node():key(""),frequency(1),left(nullptr),right(nullptr){ }
+    Node(string key) {
+      this->key = key;
+      this->left = this->right = NULL;
+      this->frequency = 1;
+    }
+    Node(Node* node) {
+      this->key = node->key;
+      this->right = node->right;
+      this->left = node->left;
+      this->frequency = node->frequency;
+    }
+    Node() :key(""), frequency(1), left(nullptr), right(nullptr) { }
 
-        void printWord() const
-        {
-            std::cout << " Word:  " << key <<setw(30)<< " Frequency: " << frequency << '\n';
-        }
-    };
+    void printWord() const
+    {
+      std::cout << " Word:  " << key << setw(30) << " Frequency: " << frequency << '\n';
+    }
+  };
 
-    //string* filename;
-    Node* root;
-    int count;
-    vector< pair<string, int> > VecForNode; //for Frequency sort
+  Node* root;
+  int count;
+  vector< pair<string, int> > VecForNode; //for Frequency sort
 
 public:
-    friend int ReadText(Dictionary&);
-    Dictionary() {
-        root = NULL;
-        count = 0;
-    }
-    ~Dictionary() {
-        //cout << "Destoryed" << endl;
-        // destroy(root);
-    }
+  Dictionary() {
+    root = NULL;
+    count = 0;
+  }
+  ~Dictionary() {
+    destroy(root);
+  }
 
-    int size() {
-        return count;
-    }
+  int size() {
+    return count;
+  }
 
-    bool isEmpty() {
-        return count == 0;
-    }
+  bool isEmpty() {
+    return count == 0;
+  }
 
-    void insert(string key) {
-        if(key!= "")
-            root = insert(root, key);
-    }
+  void insert(string key) {
+    if (key != "")
+      root = insert(root, key);
+  }
 
-    void preOrderPrint() {
-        preOrderPrint(root);
-    }
+  void preOrderPrint() {
+    preOrderPrint(root);
+  }
 
-    void search(string key) {
-        search(root, key);
-    }
+  void search(string key) {
+    search(root, key);
+  }
 
-    /*
-    void printList() {
-        levelSort(root);
+  bool isContain(string key) {
+    return isContain(root, key);
+  }
+  int getFrequency(string key) {
+    return getFrequency(root, key);
+  }
+  void removeMin() {
+    if (root != NULL) {
+      root = removeMin(root);
     }
-    */
+  }
+  void remove(string key) {
+    root = remove(root, key);
+    cout << "Successfully removed.\n";
+  }
 
-    bool isContain(string key) {
-        return isContain(root, key);
-    }
-    int getFrequency(string key) {
-        return getFrequency(root, key);
-    }
-    void removeMin() {
-        if (root != NULL) {
-            root = removeMin(root);
-        }
-    }
-    void removeMax() {
-        if (root != NULL) {
-            root = removeMax(root);
-        }
-    }
-    void remove(string key) {
-        root = remove(root, key);
-        cout << "Successfully removed.\n";
-    }
-
-    void interactiveGetModes() //MUST AFTER SORTING
+  void interactiveGetModes()
+  {
+    levelSort(root);
+    cout << "How many modes(appear most) do you want? \n";
+    int size;
+    cin >> size;
+    if (!cin) 
     {
-        cout << "How many modes(appear most) do you want? \n";
-        int size;
-        cin >> size;
-        if (!cin) {
-            throw Exception("Not number,bad input.\n");
-        }
-
-        for (int i = 0; i < size; i++) {
-            cout << "Top " << i + 1 << ": \t" << VecForNode[i].first;
-            cout << " Frequency: " << VecForNode[i].second << '\n';
-        }
-        cout << "Print Top " << size << " finished.\n";
+      throw Exception("Input is not a number,can't get the result.\n");
     }
-
-    void printList()
+    if (size > count)
     {
-        levelSort(root);
-        cout << "Words' count:\t" << VecForNode.size() << std::endl;
-        cout << "----------------------Frequency List--------------------------" << endl;
-        for (auto it = VecForNode.begin(); it != VecForNode.end(); it++)
-        {
-            cout << "|  Word: " << std::setw(30) << it->first
-                << " |  Frequency:\t" << it->second << setw(5) << " |" << endl;
-        }
-        cout << "--------------------------------------------------\n";
+        throw Exception("Overflow.Number is too big.\n");
     }
+
+    for (int i = 0; i < size; i++) 
+    {
+      cout << "Top " << i + 1 << ": \t" << VecForNode[i].first;
+      cout << " Frequency: " << VecForNode[i].second << '\n';
+    }
+    cout << "Print Top " << size << " finished.\n";
+  }
+
+  void printList()
+  {
+    levelSort(root);
+    cout << "Words' count:\t" << VecForNode.size() << std::endl;
+    cout << "----------------------Frequency List--------------------------" << endl;
+    for (auto it = VecForNode.begin(); it != VecForNode.end(); it++)
+    {
+      cout << "|  Word: " << std::setw(30) << it->first
+        << " |  Frequency:\t" << it->second << setw(5) << " |" << endl;
+    }
+    cout << "--------------------------------------------------\n";
+  }
 
 private:
 
-    Node* insert(Node*& node,string key) { 
-
-        if (node == NULL) {
-            count++;
-            return new Node(key);
-        }
-
-        if ( key.compare(node->key) >0 ) {
-            node->right = insert(node->right, key);
-        }
-        else if ( key.compare( node->key ) <0 ){
-            node->left = insert(node->left, key);
-        }
-        else
-        {
-            node->frequency++;
-        }
-        return node;
-    }
-
-    void preOrderPrint(Node* node) {
-        if (node != NULL) {
-            node->printWord();
-            preOrderPrint(node->left);
-            preOrderPrint(node->right);
-        }
-    }
-    
-    void levelSort(Node* node) 
+  Node* insert(Node*& node, string key)
+  {
+    if (node == NULL)
     {
-        VecForNode.clear();
-
-        queue<Node*>que;
-        que.push(node);
-        
-        while (!que.empty()) {
-            Node* p = que.front();
-            que.pop();
-
-            pair<string, int> temp_pair = make_pair(p->key, p->frequency);
-            VecForNode.push_back(temp_pair);
-
-            if (p->left != NULL) {
-                que.push(p->left);
-            }
-            if (p->right != NULL) {
-                que.push(p->right);
-            }
-        }
-        sort(VecForNode.begin(), VecForNode.end(), cmp_test);
-
-    }
-    void search(Node* node, string key) {
-        if (node != NULL)
-        {
-            if (node->key == key) {
-                cout << "Word is found:\n";
-                node->printWord();
-                return;
-            }
-            else if (node->key > key) {
-                return search(node->left, key);
-            }
-            else
-            {
-                return search(node->right, key);
-            }
-        }
-        else
-            throw Exception("No such word exists in your dictionary.\n");
-        
+      count++;
+      return new Node(key);
     }
 
-    int getFrequency(Node* node, string key) {
-        if (node != NULL)
-        {
-            if (node->key == key) {
-                return node->frequency;
-            }
-            else if (node->key > key) {
-                return getFrequency(node->left, key);
-            }
-            else
-            {
-                return getFrequency(node->right, key);
-            }
-        }
-        else
-            throw Exception("No such word exists in your dictionary.\n");
+    if (key.compare(node->key) > 0) {
+      node->right = insert(node->right, key);
     }
+    else if (key.compare(node->key) < 0) {
+      node->left = insert(node->left, key);
+    }
+    else
+    {
+      node->frequency++;
+    }
+    return node;
+  }
 
-    bool isContain(Node* node, string key) {
-        if (node != NULL)
-        {
-            if (node->key == key) {
-                return true;
-            }
-            else if (node->key > key) {
-                return isContain(node->left, key);
-            }
-            else {
-                return isContain(node->right, key);
-            }
-        }
-        return false;
+  void preOrderPrint(Node* node) {
+    if (node != NULL) {
+      node->printWord();
+      preOrderPrint(node->left);
+      preOrderPrint(node->right);
     }
+  }
 
-    void destroy(Node* node) {
-        if (node != NULL) {
-            destroy(node->left);
-            destroy(node->right);
-            delete node;
-            count--;
-        }
-    }
+  void levelSort(Node* node)
+  {
+    VecForNode.clear();
+    queue<Node*>que;
+    que.push(node);
 
-    Node* minimun(Node* node) {
-        if (node->left == NULL) {
-            return node;
-        }
-        return minimun(node->left);
-    }
+    while (!que.empty()) {
+      Node* p = que.front();
+      que.pop();
 
-    Node* maxmun(Node* node) {
-        if (node->right == NULL) {
-            return node;
-        }
-        return maxmun(node->right);
-    }
+      pair<string, int> temp_pair = make_pair(p->key, p->frequency);
+      VecForNode.push_back(temp_pair);
 
-    Node* removeMin(Node* node) {
-        if (node->left == NULL) {
-            Node* rightNode = node->right;
-            delete node;
-            count--;
-            return rightNode;
-        }
-        else
-        {
-            node->left = removeMin(node->left);
-            return node;
-        }
+      if (p->left != NULL) {
+        que.push(p->left);
+      }
+      if (p->right != NULL) {
+        que.push(p->right);
+      }
     }
+    sort(VecForNode.begin(), VecForNode.end(), cmp_test);
+  }
 
-    Node* removeMax(Node* node) {
-        if (node->right == NULL) {
-            Node* leftNode = node->left;
-            delete node;
-            count--;
-            return leftNode;
-        }
-        else
-        {
-            node->right = removeMax(node->right);
-            return node;
-        }
+  void search(Node* node, string key) 
+  {
+    if (node != NULL)
+    {
+      if (node->key == key) {
+        cout << "Word is found:\n";
+        node->printWord();
+        return;
+      }
+      else if (node->key > key) {
+        return search(node->left, key);
+      }
+      else
+      {
+        return search(node->right, key);
+      }
     }
+    else
+      throw Exception("No such word exists in your dictionary.\n");
 
-    Node* remove(Node* node, string key) {
-        if (node == NULL) {
-            throw Exception("No such word contains.\n");
-        }
-        if (node->key == key) {
-            if (node->left == NULL) {
-                Node* rightNode = node->right;
-                delete node;
-                count--;
-                return rightNode;
-            }
-            if (node->right == NULL) {
-                Node* leftNode = node->left;
-                delete node;
-                count--;
-                return leftNode;
-            }
-            Node* s = new Node(minimun(node->right)); //用s复制该节点的右子树的最小值的那个节点
-            count++;
-            s->right = removeMin(node->right);//把右子树那个最小值删掉并且做s的右子树
-            s->left = node->left;
-            delete node;
-            count--;
-            return s;
-        }
-        else if (node->key > key) {
-            node->left = remove(node->left, key);
-            return node;
-        }
-        else {
-            node->right = remove(node->right, key);
-            return node;
-        }
+  }
+
+  int getFrequency(Node* node, string key) 
+  {
+    if (node != NULL)
+    {
+      if (node->key == key) {
+        return node->frequency;
+      }
+      else if (node->key > key) {
+        return getFrequency(node->left, key);
+      }
+      else
+      {
+        return getFrequency(node->right, key);
+      }
     }
+    else
+      throw Exception("No such word exists in your dictionary.\n");
+  }
+
+  bool isContain(Node* node, string key) 
+  {
+    if (node != NULL)
+    {
+      if (node->key == key) {
+        return true;
+      }
+      else if (node->key > key) {
+        return isContain(node->left, key);
+      }
+      else {
+        return isContain(node->right, key);
+      }
+    }
+    return false;
+  }
+
+  void destroy(Node* node) 
+  {
+    if (node != NULL) {
+      destroy(node->left);
+      destroy(node->right);
+      delete node;
+      count--;
+    }
+  }
+
+  Node* minimun(Node* node) {
+    if (node->left == NULL) {
+      return node;
+    }
+    return minimun(node->left);
+  }
+
+  Node* maxmun(Node* node) {
+    if (node->right == NULL) {
+      return node;
+    }
+    return maxmun(node->right);
+  }
+
+  Node* removeMin(Node* node) {
+    if (node->left == NULL) {
+      Node* rightNode = node->right;
+      delete node;
+      count--;
+      return rightNode;
+    }
+    else
+    {
+      node->left = removeMin(node->left);
+      return node;
+    }
+  }
+  Node* remove(Node* node, string key) 
+  {
+    if (node == NULL) {
+      throw Exception("No such word contains.\n");
+    }
+    if (node->key == key) {
+      if (node->left == NULL) {
+        Node* rightNode = node->right;
+        delete node;
+        count--;
+        return rightNode;
+      }
+      if (node->right == NULL) {
+        Node* leftNode = node->left;
+        delete node;
+        count--;
+        return leftNode;
+      }
+      Node* s = new Node(minimun(node->right));
+      count++;
+      s->right = removeMin(node->right);
+      s->left = node->left;
+      delete node;
+      count--;
+      return s;
+    }
+    else if (node->key > key) {
+      node->left = remove(node->left, key);
+      return node;
+    }
+    else {
+      node->right = remove(node->right, key);
+      return node;
+    }
+  }
 };
+
+bool cmp_test(const pair<string, int>& a, const pair<string, int>& b)
+{
+    return a.second > b.second;
+}
